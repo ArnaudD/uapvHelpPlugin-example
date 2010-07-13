f764c8781e877991490f7e12d15de3c22bbfd7a9
ArnaudD <arnaud.didry@gmail.com>
2010-07-13 17:46:05 +0200

    Added a basic symfony project

---
+++ apps/frontend/modules/myModule/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * myModule actions.
+ *
+ * @package    uapvHelpPlugin-example
+ * @subpackage myModule
+ * @author     Arnaud Didry
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class myModuleActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
